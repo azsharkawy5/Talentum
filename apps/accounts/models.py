7f59086e5d223478bfd2d72fd068877f0774ddbe
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-08-29 14:12:23 +0300

    feat: add User model and admin registration for Accounts app

---
+++ apps/accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User (AbstractUser):
+
+    email=models.EmailField(max_length=255, unique=True) 
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    is_email_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
